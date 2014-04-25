a975ecbcf38af638a0c0b9a3cd56c2d3a3a53689
alexclp <alexandru.clapa@gmail.com>
2014-04-25 13:48:15 +0300

    Added frameworks, files.

---
+++ AccelerationAnalyzer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AccelerationAnalyzer
+//
+//  Created by Alexandru Clapa on 25.04.2014.
+//  Copyright (c) 2014 Alexandru Clapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
